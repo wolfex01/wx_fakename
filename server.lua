4caebf16f30d19e349d4c2ec4839c4ca1c93e068
wolfex <91018450+wolfex01@users.noreply.github.com>
2023-02-14 21:29:23 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,9 +1,15 @@
 ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-RegisterServerEvent("newName")
-AddEventHandler("newName", function (newName)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    xPlayer.setName(newName)
+-- eseménykezelő az ESX objektum lekéréséhez
+TriggerEvent('esx:getSharedObject', function(obj)
+    ESX = obj
 end)
 
+-- eseménykezelő a "newName" szerveroldali eseményhez
+AddEventHandler('newName', function(newName)
+    -- lekérjük a játékost az azonosító alapján
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    -- beállítjuk az új nevet
+    xPlayer.setName(newName)
+end)
